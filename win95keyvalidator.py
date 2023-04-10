3203efd9eba2e37b363f83a94a8be404a3f478fb
s1522711 <s1522711@gmail.com>
2023-04-10 22:22:26 +0300

    the validator now replies with the issues of the keys if they are invalid

--- win95keyvalidator.py
+++ win95keyvalidator.py
@@ -18,25 +18,48 @@ try:
                 box3 = args.key[key][10] + args.key[key][11] + args.key[key][12] + args.key[key][13] + args.key[key][14] + args.key[key][15] + args.key[key][16]
                 box4 = args.key[key][18] +args.key[key][19] + args.key[key][20] + args.key[key][21] + args.key[key][22]
                 validity_score = 0
+                errors = ""
 
                 if 1 <= int(box1p1) <= 366:
                     validity_score += 1
+                else:
+                    errors += "first 3 characters are not between 1 and 366, "
+
                 if box1p2 in ["95","96","97","98","99","00","01","02"]:
                     validity_score += 1
+                else:
+                    errors += "last 2 characters of the first box are not in whitelist, "
+
                 if args.key[key][5] == "-" and args.key[key][9] == "-" and args.key[key][17] == "-":
                     validity_score += 1
-                if sum(int(digit) for digit in str(box3)) % 7 == 0 and box3[0] == "0":
+                else:
+                    errors += "characters number 6 or 10 or 18 are not dashes, "
+
+                if sum(int(digit) for digit in str(box3)) % 7 == 0:
                     validity_score += 1
+                else:
+                    errors += "sum of the second box isnt devidable by 7, "
+
                 if len(box4) == 5:
                     validity_score += 1
+                else:
+                    errors += "lenght of the fourth box isnt 5, "
+
                 if len(args.key[key]) == 23:
                     validity_score += 1
+                else:
+                    errors += "length of key isnt 23, "
+                
+                if box3[0] == "0":
+                    validity_score += 1
+                else:
+                    errors += "first character in box3 isnt 0."
                 
                 #print(validity_score)
-                if validity_score == 6:
+                if validity_score == 7:
                     print("PASS - "+args.key[key])
                 else:
-                    print("FAIL - "+args.key[key])
+                    print("FAIL - "+args.key[key]+" - "+errors)
 
 
                 #01234567890
@@ -44,20 +67,32 @@ try:
                 box1 = args.key[key][0] + args.key[key][1] + args.key[key][2]
                 box2 = args.key[key][4] + args.key[key][5] + args.key[key][6] + args.key[key][7] + args.key[key][8] + args.key[key][9] + args.key[key][10]
                 validity_score = 0
+                errors = ""
 
                 if box1 not in ["333","444","555","666","777","888","999"]:
                     validity_score += 1
-               # if args.key[key][3] == "-":
-              #      validity_score += 1
-                if sum(int(digit) for digit in str(box2)) % 7 == 0 and int(box2[len(box2)-1]) < 8:
+                else:
+                    errors =+ "first 3 characters in blacklist, "
+
+                if sum(int(digit) for digit in str(box2)) % 7 == 0:
                     validity_score += 1
+                else:
+                    errors += "sum of numbers starting from the 4th character isnt devidible by 7, "
+
                 if len(args.key[key]) == 11:
                     validity_score += 1
+                else:
+                    errors += "length of key isnt 11, "
                 
-                if validity_score == 3:
+                if int(box2[len(box2)-1]) < 8:
+                    validity_score += 1
+                else:
+                    errors += "last character is bigger than 7."
+                
+                if validity_score == 4:
                     print("PASS - "+args.key[key])
                 else:
-                    print("FAIL - "+args.key[key])
+                    print("FAIL - "+args.key[key]+" - "+errors)
         else:
             print("FAIL (too short) - "+args.key[key])
 except ValueError:
